5e5de5789867d99895d88d51c6501abcc705d8c3
Tasuku HIRAISHI <Tasuku HIRAISHI@localhost>
2018-12-12 10:09:22 +0900

    Fixed comments in bem_file.h

--- data/bem_file.h
+++ data/bem_file.h
@@ -12,27 +12,28 @@ enum bi_format {
 struct bem_input {
   int64_t nNode;            // # of nodes
   double (*coordOfNode)[3]; // coordOfnode[i][d] is d-th dimension of i-th coordinate
-                            // where 0 < i < nNode and 0 < d < 3
+                            // where 0 <= i < nNode and 0 <= d < 3
   int64_t nFace;            // # of faces
   int64_t nNodePerFace;     // # of nodes that form each face     
   int64_t nIFValue;         // # of integer parameter values for each face
   int64_t nDFValue;         // # of double parameter values for each face
   int64_t *idOfFace;        // idOfFace[i*nNodePerFace+j] is j-th node ID of i-th face
-                            // where 0 < i < nFace and 0 < j < jNodePerFace
+                            // where 0 <= i < nFace and 0 <= j < jNodePerFace
   double (*coordOfFace)[3]; // coordOfFace[i][d] is d-th dimension of i-th face's center
-                            // where 0 < i < nFace and 0 < d < 3
+                            // where 0 <= i < nFace and 0 <= d < 3
   int64_t *IFValue;         // IFValue[i*nFace+j] is i-th integer parameter value
-                            // of j-th face where 0<i<nFace and 0<j<nIFValue
+                            // of j-th face where 0 <= i < nFace and 0 <= j < nIFValue
   double *DFValue;          // DFValue[i*nFace+j] is i-th double parameter value
-                            // of j-th face where 0<i<nFace and 0<j<nIDValue
+                            // of j-th face where 0 <= i <nFace and 0 <= j < nIDValue
 };
 
+/* functions defined in bem_file.c */
 void print_bem_input (FILE* fp, struct bem_input* pbin, enum bi_format fmt);
 enum bi_format read_bem_input (FILE* fp, struct bem_input* pbin, enum bi_format fmt);
 enum bi_format open_and_read_bem_input (char *ifile, struct bem_input* pbin, enum bi_format fmt);
 
 
-/* functions in bem_convert.c */
+/* functions defined in bem_aux.c */
 double show_elapsed_time (struct timeval *tv1, struct timeval *tv2);
 void open_write_show (const int flag, struct bem_input *pbin,
 		      const enum bi_format fmt, const char* ofile);
