6897554e7b2028afb50e0ed37854c2696defdd1c
Simon Bai <bzy19921017@gmail.com>
2019-01-21 20:50:36 +0900

    fix a bug in tascell and change threshold for parallel recursive call to the number of elements from tree level

--- jobscript.sh
+++ jobscript.sh
@@ -2,8 +2,8 @@
 #============ PBS Options ============
 #QSUB -q gr10034b
 #QSUB -ug gr10034
-#QSUB -W 0:10
+#QSUB -W 1:00
 #QSUB -A p=1:t=36:c=36:m=120G
 #============ Shell Script ===========
 
-mpiexec.hydra ./hdcm 36
+mpiexec.hydra ./hdcm data_pro1804/input_100ts.txt_pb_1.5_14.bin
